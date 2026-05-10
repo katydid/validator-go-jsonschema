3a33ab2e98c7441bfbf320c45a97d628c82dd008
Walter Schulze <awalterschulze@gmail.com>
2026-05-10 17:00:44 +0100

    add blogpost test

---
+++ jsonschema/blogpost_test.go
@@ -0,0 +1,98 @@
+// Copyright 2026 Walter Schulze
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package jsonschema
+
+import "testing"
+
+const SchemaJSONSchemaExampleBlogPost = `{
+  "$id": "https://example.com/blog-post.schema.json",
+  "$schema": "https://json-schema.org/draft/2020-12/schema",
+  "description": "A representation of a blog post",
+  "definitions": {
+    "user-profile": {
+      "type": "object",
+      "required": ["Username", "Email"],
+      "properties": {
+        "Username": {
+          "type": "string"
+        },
+        "Email": {
+          "type": "string",
+          "format": "email"
+        },
+        "FullName": {
+          "type": "string"
+        },
+        "Age": {
+          "type": "integer",
+          "minimum": 0
+        },
+        "Location": {
+          "type": "string"
+        },
+        "Interests": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        }
+      }
+    }
+  },
+  "type": "object",
+  "required": ["Title", "Content", "Author"],
+  "properties": {
+    "Title": {
+      "type": "string"
+    },
+    "Content": {
+      "type": "string"
+    },
+    "PublishedDate": {
+      "type": "string",
+      "format": "date-time"
+    },
+    "Author": {
+      "$ref": "#/definitions/user-profile"
+    },
+    "Tags": {
+      "type": "array",
+      "items": {
+        "type": "string"
+      }
+    }
+  }
+}`
+
+var blogpostPasses = []string{
+	`{"Title":"9CAoZ","Content":"jsUMl7","PublishedDate":"2000-11-11T00:12:03Z","Author":{"Username":"h78o02X1","Email":"xzvcwvj@hotmail.com","FullName":"Fz","Age":4937305690741089630,"Location":"oWo","Interests":["k"]},"Tags":["c","z","","U76zzqj"]}`,
+}
+
+func TestBlogpost(t *testing.T) {
+	g, err := newGrammar([]byte(SchemaJSONSchemaExampleBlogPost))
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("translated to: %v", g.String())
+	for _, input := range blogpostPasses {
+		m, err := MatchBytes([]byte(SchemaJSONSchemaExampleBlogPost), []byte(input))
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !m {
+			t.Errorf("expected true, but got no match for %s", input)
+		}
+	}
+}
